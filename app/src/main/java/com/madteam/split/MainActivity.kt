1003ebaa79a0104d419a71066061e6057c7e9567
adriafernandezarans <adrifernandevs@gmail.com>
2023-12-22 19:26:57 +0100

    [feat] Created activity and implemented dependencies

---
+++ app/src/main/java/com/madteam/split/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.madteam.split
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
