64a8ac47bc7fa35aee2ffaf8799d9e0df17f4c30
adriafernandezarans <adrifernandevs@gmail.com>
2024-01-06 17:09:20 +0100

    [feat] WIP: Implementing design of my groups screen

---
+++ app/src/main/java/com/madteam/split/ui/screens/mygroups/ui/MyGroupsScreen.kt
@@ -0,0 +1,105 @@
+package com.madteam.split.ui.screens.mygroups.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.madteam.split.R
+import com.madteam.split.ui.theme.PrimaryLargeButton
+import com.madteam.split.ui.theme.SecondaryLargeButton
+import com.madteam.split.ui.theme.SplitTheme
+
+@Composable
+fun MyGroupsScreen() {
+
+    Scaffold(
+        containerColor = SplitTheme.colors.neutral.backgroundExtraWeak
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            MyGroupsContent()
+        }
+    }
+}
+
+@Composable
+fun MyGroupsContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        MyGroupsTopBar()
+        Spacer(modifier = Modifier.size(24.dp))
+        PrimaryLargeButton(
+            onClick = { /*TODO*/ },
+            text = R.string.received_an_invitation
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        Text(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            text = stringResource(id = R.string.or),
+            style = SplitTheme.typography.body.xxl,
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        SecondaryLargeButton(
+            onClick = { /*TODO*/ },
+            text = R.string.create_a_new_group
+        )
+    }
+}
+
+@Composable
+fun MyGroupsTopBar() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = stringResource(id = R.string.your_groups),
+                style = SplitTheme.typography.heading.m,
+                color = SplitTheme.colors.neutral.textTitle,
+            )
+            Icon(
+                modifier = Modifier
+                    .size(48.dp),
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = null
+            )
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun MyGroupsScreenPreview() {
+    MyGroupsScreen()
+}
