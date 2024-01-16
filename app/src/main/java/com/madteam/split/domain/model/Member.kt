4006ac7344085bdff472d74c16c31d396ecfe0fe
adriafernandezarans <adrifernandevs@gmail.com>
2024-01-16 07:51:44 +0100

    [feat] WIP: Implementing Blobs

---
+++ app/src/main/java/com/madteam/split/domain/model/Member.kt
@@ -0,0 +1,11 @@
+package com.madteam.split.domain.model
+
+data class Member(
+    val id: Int,
+    val name: String,
+    val profileImage: String?,
+    val user: Int?,
+    val color: String,
+    val joinedDate: String,
+    val groupId: Int,
+)
---
+++ app/src/main/java/com/madteam/split/ui/theme/Members.kt
@@ -0,0 +1,16 @@
+package com.madteam.split.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MembersHorizontalList(
+
+) {
+}
+
+@Preview
+@Composable
+fun MembersHorizontalListPreview() {
+    MembersHorizontalList()
+}
