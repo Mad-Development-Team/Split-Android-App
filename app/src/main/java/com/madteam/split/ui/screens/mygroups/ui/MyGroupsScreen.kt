64a86106dbae9a44e770cdeba9136cb3325c2c50
adriafernandez <adria.fernandez@col.vueling.com>
2024-01-21 20:31:16 +0100

    [feat] WIP: Implementing Group list item

--- app/src/main/java/com/madteam/split/ui/screens/mygroups/ui/MyGroupsScreen.kt
+++ app/src/main/java/com/madteam/split/ui/screens/mygroups/ui/MyGroupsScreen.kt
@@ -1,5 +1,7 @@
 package com.madteam.split.ui.screens.mygroups.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,20 +11,32 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.madteam.split.R
+import com.madteam.split.domain.model.Group
+import com.madteam.split.domain.model.Member
 import com.madteam.split.domain.model.User
 import com.madteam.split.ui.navigation.Screens
 import com.madteam.split.ui.screens.mygroups.state.MyGroupsUIState
@@ -133,10 +147,126 @@ fun MyGroupsTopBar(
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+private fun GroupListItem(
+    group: Group,
+) {
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(124.dp),
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.elevatedCardElevation(
+            defaultElevation = 8.dp
+        ),
+        colors = CardDefaults.elevatedCardColors(
+            containerColor = SplitTheme.colors.secondary.backgroundMedium
+        )
+    ) {
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            val (backgroundImage, degraded, groupName, members) = createRefs()
+            if (group.bannerImage.isNotEmpty()) {
+                GlideImage(
+                    modifier = Modifier
+                        .constrainAs(backgroundImage) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                        },
+                    contentScale = ContentScale.Crop,
+                    model = group.bannerImage,
+                    contentDescription = stringResource(id = R.string.group_banner_image)
+                )
+            } else {
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .rotate(180f)
+                        .constrainAs(backgroundImage) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                        },
+                    contentScale = ContentScale.Crop,
+                    painter = painterResource(id = R.drawable.default_group_banner_image),
+                    contentDescription = stringResource(id = R.string.group_banner_image),
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Black,
+                                Color.Transparent
+                            ),
+                            startY = Float.POSITIVE_INFINITY,
+                            endY = 0f
+                        )
+                    )
+            )
+        }
+
+    }
+}
+
 @Preview
 @Composable
-fun MyGroupsScreenPreview() {
-    MyGroupsScreen(
-        navController = rememberNavController()
+fun GroupListItemPreview() {
+    GroupListItem(
+        group = Group(
+            id = 10,
+            name = "Amsterdam",
+            description = "",
+            inviteCode = "R2PZMT",
+            image = "",
+            bannerImage = "",
+            createdDate = "2024-01-21 18:28:42",
+            members = listOf(
+                Member(
+                    id = 21,
+                    name = "adria",
+                    profileImage = "",
+                    user = 5,
+                    color = "",
+                    joinedDate = "2024-01-21 18:28:42",
+                    groupId = 10
+                ),
+                Member(
+                    id = 22,
+                    name = "david",
+                    profileImage = "",
+                    user = null,
+                    color = "",
+                    joinedDate = "2024-01-21 18:28:42",
+                    groupId = 10
+                ),
+                Member(
+                    id = 23,
+                    name = "Berni",
+                    profileImage = "",
+                    user = null,
+                    color = "",
+                    joinedDate = "2024-01-21 18:28:42",
+                    groupId = 10
+                ),
+                Member(
+                    id = 24,
+                    name = "Oscar",
+                    profileImage = "",
+                    user = null,
+                    color = "",
+                    joinedDate = "2024-01-21 18:28:42",
+                    groupId = 10
+                ),
+            )
+        )
     )
 }
