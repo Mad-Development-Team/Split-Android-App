329f781a644f8afed6a47bf6116f16dfbfdd4822
adriafernandez <adria.fernandez@col.vueling.com>
2024-01-28 20:05:02 +0100

    [feat] WIP: Working on Group expenses summary section

--- app/src/main/java/com/madteam/split/ui/screens/groupexpenses/ui/GroupExpensesScreen.kt
+++ app/src/main/java/com/madteam/split/ui/screens/groupexpenses/ui/GroupExpensesScreen.kt
@@ -1,18 +1,26 @@
 package com.madteam.split.ui.screens.groupexpenses.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.madteam.split.R
 import com.madteam.split.ui.navigation.Screens
 import com.madteam.split.ui.screens.group.viewmodel.GroupViewModel
 import com.madteam.split.ui.screens.groupexpenses.state.GroupExpensesUIEvent
@@ -23,6 +31,7 @@ import com.madteam.split.ui.theme.GroupsListModalBottomSheet
 import com.madteam.split.ui.theme.SplitTheme
 import com.madteam.split.utils.ui.BackPressHandler
 import com.madteam.split.utils.ui.navigateWithPopUpTo
+import java.math.BigDecimal
 
 @Composable
 fun GroupExpensesScreen(
@@ -92,7 +101,66 @@ fun GroupExpensesScreen(
 
 @Composable
 fun GroupExpensesContent() {
-    Text(text = "Group expenses")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+
+    }
+}
+
+@Composable
+fun GroupExpensesSummarySection(
+    modifier: Modifier = Modifier,
+    values: Map<String, BigDecimal>,
+    currency: String,
+) {
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        val (blob, amount, decimalAmount, currency) = createRefs()
+        val totalExpenses = values.values.elementAt(0)
+        val lastDay = values.values.elementAt(1)
+        val lastWeek = values.values.elementAt(2)
+        Image(
+            modifier = Modifier
+                .constrainAs(blob) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .size(150.dp),
+            painter = painterResource(id = R.drawable.blob2),
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier
+                .constrainAs(amount) {
+                    top.linkTo(blob.top)
+                    bottom.linkTo(blob.bottom)
+                    start.linkTo(blob.start)
+                    end.linkTo(blob.end)
+                },
+            text = totalExpenses.toInt().toString(),
+            color = SplitTheme.colors.neutral.textExtraWeak,
+            style = SplitTheme.typography.display.s
+        )
+    }
+}
+
+@Preview
+@Composable
+fun GroupExpensesSummarySectionPreview() {
+    GroupExpensesSummarySection(
+        values = mapOf(
+            "Total expenses" to BigDecimal(1546.33),
+            "Last day" to BigDecimal(54.87),
+            "Last week" to BigDecimal(349)
+        ),
+        currency = "€"
+    )
 }
 
 @Preview
