d4953a0239f6345e00a632a850a07bffdc10e61f
adriafernandez <adria.fernandez@col.vueling.com>
2024-01-28 19:42:41 +0100

    [feat] Implemented topBar on group expenses screen

---
+++ app/src/main/java/com/madteam/split/ui/screens/groupexpenses/state/GroupExpensesUIState.kt
@@ -0,0 +1,5 @@
+package com.madteam.split.ui.screens.groupexpenses.state
+
+data class GroupExpensesUIState(
+    val groupsModalIsVisible: Boolean = false,
+)
--- app/src/main/java/com/madteam/split/ui/screens/groupexpenses/ui/GroupExpensesScreen.kt
+++ app/src/main/java/com/madteam/split/ui/screens/groupexpenses/ui/GroupExpensesScreen.kt
@@ -6,25 +6,78 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.madteam.split.ui.navigation.Screens
+import com.madteam.split.ui.screens.group.viewmodel.GroupViewModel
+import com.madteam.split.ui.screens.groupexpenses.state.GroupExpensesUIEvent
+import com.madteam.split.ui.screens.groupexpenses.viewmodel.GroupExpensesViewModel
 import com.madteam.split.ui.theme.DSBottomNavigation
+import com.madteam.split.ui.theme.GroupNavigationTopAppBar
+import com.madteam.split.ui.theme.GroupsListModalBottomSheet
 import com.madteam.split.ui.theme.SplitTheme
 import com.madteam.split.utils.ui.BackPressHandler
+import com.madteam.split.utils.ui.navigateWithPopUpTo
 
 @Composable
 fun GroupExpensesScreen(
     navController: NavController,
+    commonViewModel: GroupViewModel = hiltViewModel(),
+    viewModel: GroupExpensesViewModel = hiltViewModel(),
 ) {
     BackPressHandler {
         //Do nothing on back press
     }
+
+    val commonState by commonViewModel.state.collectAsStateWithLifecycle()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    if (state.groupsModalIsVisible) {
+        GroupsListModalBottomSheet(
+            groupsList = commonState.userGroups,
+            currentGroupId = commonState.currentGroupId!!,
+            onClose = {
+                viewModel.onEvent(
+                    GroupExpensesUIEvent.ShowGroupsModal(
+                        show = false
+                    )
+                )
+            },
+            onGroupSelected = {},
+            onNavigateHomeSelected = {
+                GroupExpensesUIEvent.ShowGroupsModal(
+                    show = false
+                )
+                navController.navigateWithPopUpTo(
+                    route = Screens.MyGroupsScreen.route,
+                    popUpTo = Screens.GroupExpensesScreen.route,
+                    inclusive = true
+                )
+            }
+        )
+    }
+
     Scaffold(
         containerColor = SplitTheme.colors.neutral.backgroundExtraWeak,
         bottomBar = {
             DSBottomNavigation(navController = navController)
+        },
+        topBar = {
+            GroupNavigationTopAppBar(
+                currentGroup = commonState.userGroups.first { it.id == commonState.currentGroupId },
+                onExpandClicked = {
+                    viewModel.onEvent(
+                        GroupExpensesUIEvent.ShowGroupsModal(
+                            show = true
+                        )
+                    )
+                }
+            )
         }
     ) {
         Box(
