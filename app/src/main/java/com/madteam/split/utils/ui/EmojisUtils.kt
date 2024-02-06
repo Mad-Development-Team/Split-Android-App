779db216fe32b9a6e415b0db7a3303ca5d23a7cd
adriafernandez <adria.fernandez@col.vueling.com>
2024-02-06 07:40:35 +0100

    [feat] WIP: Working on emoji dialog picker

--- app/src/main/java/com/madteam/split/utils/ui/EmojisUtils.kt
+++ app/src/main/java/com/madteam/split/utils/ui/EmojisUtils.kt
@@ -5,39 +5,28 @@ import com.madteam.split.domain.model.Currency
 
 fun getFlagByCurrency(currency: Currency?): Int {
     return when {
-        /* currency != null -> {
+         currency != null -> {
              when (currency.currency) {
-                "EUR" -> R.drawable.emoji_europe_flag
-                "GBP" -> R.drawable.emoji_uk_flag
-                "CHF" -> R.drawable.emoji_switzerland_flag
-                "SEK" -> R.drawable.emoji_sweden_flag
-                "NOK" -> R.drawable.emoji_norway_flag
-                "DKK" -> R.drawable.emoji_denmark_flag
-                "PLN" -> R.drawable.emoji_poland_flag
-                "USD" -> R.drawable.emoji_usa_flag
-                "CAD" -> R.drawable.emoji_canada_flag
-                "MXN" -> R.drawable.emoji_mexico_flag
-                "BRL" -> R.drawable.emoji_brazil_flag
-                "ARS" -> R.drawable.emoji_argentina_flag
-                "COP" -> R.drawable.emoji_colombia_flag
-                "PEN" -> R.drawable.emoji_peru_flag
-                else -> R.drawable.emoji_europe_flag
-            }
-        } */ //TODO: Change for real flags
+                 "EUR" -> R.drawable.emoji_flags_flageuropeanunion
+                 "GBP" -> R.drawable.emoji_flags_flagunitedkingdom
+                 "CHF" -> R.drawable.emoji_flags_flagswitzerland
+                 "SEK" -> R.drawable.emoji_flags_flagsweden
+                 "NOK" -> R.drawable.emoji_flags_flagbouvetisland
+                 "DKK" -> R.drawable.emoji_flags_flagdenmark
+                 "PLN" -> R.drawable.emoji_flags_flagpoland
+                 "USD" -> R.drawable.emoji_flags_flagusoutlyingislands
+                 "CAD" -> R.drawable.emoji_flags_flagcanada
+                 "MXN" -> R.drawable.emoji_flags_flagmexico
+                 "BRL" -> R.drawable.emoji_flags_flagbrazil
+                 "ARS" -> R.drawable.emoji_flags_flagargentina
+                 "COP" -> R.drawable.emoji_flags_flagcolombia
+                 "PEN" -> R.drawable.emoji_flags_flagperu
+                 else -> R.drawable.emoji_flags_flageuropeanunion
+             }
+         }
 
         else -> {
-            R.drawable.emoji_animals_nature_badger
-        }
-    }
-}
-
-fun getAllEmojis(): List<Int> {
-    val fieldList = R.drawable::class.java.fields
-    return fieldList.filter { it.name.startsWith("emoji_") }.mapNotNull { field ->
-        try {
-            field.getInt(null)
-        } catch (e: Exception) {
-            null
+            R.drawable.emoji_flags_flageuropeanunion
         }
     }
 }
@@ -60,7 +49,7 @@ fun getEmojiByName(name: String): Int? {
             try {
                 field.getInt(null)
             } catch (e: Exception) {
-                R.drawable.emoji_animals_nature_badger //TODO: Replace for an emoji that shows not found
+                R.drawable.emoji_symbols_questionmark
             }
         }
 }
